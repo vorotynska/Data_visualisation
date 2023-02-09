e449aa3898791534d62c0dd3706eb0df1bc85a0b
vorotynska <109528111+vorotynska@users.noreply.github.com>
2023-02-09 18:43:14 +0200

    commit

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
 const w = 500;
 const h = 100;
 
-const svg = d3.select("body")
+var svg = d3.select("main")
     .append("svg")
     .attr("width", w)
     .attr("height", h);
@@ -81,3 +81,94 @@ svg.selectAll("text")
     .text((d, i) => d)
     .style('font-size', '25px')
     .attr('fill', 'red')
+
+
+//----------scaleLinear()
+
+const scale = d3.scaleLinear() // Створіть шкалу тут
+    .domain([250, 500])
+    .range([10, 150]);
+
+const output = scale(50); // Викличте шкалу з аргументом тут
+
+d3.select("body")
+    .append("h2")
+    .text(output);
+
+// max min 
+const positionData = [
+    [1, 7, -4],
+    [6, 3, 8],
+    [2, 9, 3]
+]
+const put = d3.max(positionData, d => d[2]); // Змініть цей рядок
+
+d3.select("body")
+    .append("h2")
+    .text(put)
+
+//---dynamic scale
+
+const dataset1 = [
+    [34, 78],
+    [109, 280],
+    [310, 120],
+    [79, 411],
+    [420, 220],
+    [233, 145],
+    [333, 96],
+    [222, 333],
+    [78, 320],
+    [21, 123]
+];
+
+const w1 = 500;
+const h1 = 500;
+
+// Відступ між межею полотна SVG та графіком
+const padding = 60;
+
+// Створіть шкалу x і y
+const xScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset, (d) => d[0])])
+    .range([padding, w - padding]);
+
+const yScale = d3.scaleLinear()
+    .domain([0, d3.max(dataset, (d) => d[1])])
+    .range([h - padding, padding]);
+
+const svg = d3.select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+    // Додайте свій код під цим рядком
+
+    .attr("cx", (d) => xScale(d[0]))
+    .attr("cy", (d) => yScale(d[1]))
+    .attr('r', 5);
+
+// Додайте свій код над цим рядком
+
+svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text((d) => (d[0] + ", " +
+        d[1]))
+    // Додайте свій код під цим рядком
+    .attr("x", (d) => xScale(d[0] + 10))
+    .attr("y", (d) => yScale(d[1]));
+
+const xAxis = d3.axisBottom(xScale);
+// Додайте свій код під цим рядком
+const yAxis = undefined;
+// Додайте свій код над цим рядком
+
+svg.append("g")
+    .attr("transform", "translate(0," + (h - padding) + ")")
+    .call(xAxis);
